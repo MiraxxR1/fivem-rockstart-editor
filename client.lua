09f996dffbb38be5fb56154877657d3eba3f168e
MiraxxR <75571297+MiraxxR1@users.noreply.github.com>
2023-01-23 22:18:03 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,62 +1,94 @@
+--[[
+	String.state =
+		Simple 
+		Complete
+]]
+--
+
 local String = {
-	{
-		name = "start_recording",
-		description = "Start recording",
-		key = "F1",
-		action = function()
-			if not IsRecording() then
-				StartRecording(1)
-				print("Starting recording!")
-			else
-				print("You are already recording!")
-			end
-		end,
+	state = "Simple",
+	data = {
+		{
+			name = "start_recording",
+			description = "Start recording",
+			key = "F1",
+			action = function()
+				if not IsRecording() then
+					StartRecording(1)
+				end
+			end,
+		},
+		{
+			name = "open_rockstar_editor",
+			description = "Open rockstart editor",
+			key = "F2",
+			action = function()
+				if not IsRecording() then
+					ActivateRockstarEditor()
+				end
+			end,
+		},
+		{
+			name = "stop_record_save",
+			description = "Stop recording and save",
+			key = "F3",
+			action = function()
+				if IsRecording() then
+					StopRecordingAndSaveClip()
+				end
+			end,
+		},
+		{
+			name = "stop_record_discard",
+			description = "Stop recording and Discard",
+			key = "F4",
+			action = function()
+				if IsRecording() then
+					StopRecordingAndDiscardClip()
+				end
+			end,
+		},
 	},
-	{
-		name = "open_rockstar_editor",
-		description = "Open rockstart editor",
-		key = "F2",
-		action = function()
-			if not IsRecording() then
-				ActivateRockstarEditor()
-			else
-				print("You are recording, stop your recording first")
-			end
-		end,
-	},
-	{
-		name = "stop_record_save",
-		description = "Stop recording and save",
-		key = "F3",
-		action = function()
-			if IsRecording() then
-				StopRecordingAndSaveClip()
-				print("Stop recording and save !")
-			else
-				print("You are not recording!")
-			end
-		end,
-	},
-	{
-		name = "stop_record_discard",
-		description = "Stop recording and Discard",
-		key = "F4",
-		action = function()
-			if IsRecording() then
-				StopRecordingAndDiscardClip()
-				print("Stop recording and Discard !")
-			else
-				print("You are not recording!")
-			end
-		end,
+	data2 = {
+		{
+			name = "start_recording_and_save",
+			description = "Start recording and save",
+			key = "F1",
+			action = function()
+				if IsRecording() then
+					StopRecordingAndSaveClip()
+				else
+					StartRecording(1)
+				end
+			end,
+		},
+		{
+			name = "open_rockstar_editor",
+			description = "Open rockstart editor",
+			key = "F2",
+			action = function()
+				if not IsRecording() then
+					ActivateRockstarEditor()
+				end
+			end,
+		},
 	},
 }
 
 Citizen.CreateThread(function()
-	for index, data in pairs(String) do
-		RegisterKeyMapping(data.name, data.description, "keyboard", data.key)
-		RegisterCommand(data.name, function()
-			data.action()
-		end, false)
+	if String.state == "Simple" then
+		for index, data in pairs(String.data2) do
+			RegisterKeyMapping(data.name, data.description, "keyboard", data.key)
+			RegisterCommand(data.name, function()
+				data.action()
+			end, false)
+		end
+	elseif String.state == "Complete" then
+		for index, data in pairs(String.data) do
+			RegisterKeyMapping(data.name, data.description, "keyboard", data.key)
+			RegisterCommand(data.name, function()
+				data.action()
+			end, false)
+		end
 	end
 end)
