59bf55f74c1ecaebfcfd59c8c62cca396c9b96a4
MiraxxR <75571297+MiraxxR1@users.noreply.github.com>
2023-01-23 22:05:06 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+name 'fivem-rockstart-editor'
+author 'MiraxxR'
+contact 'MiraxxR#0001'
+repository 'https://github.com/MiraxxR1/fivem-rockstart-editor'
+discord 'https://discord.gg/wJmnKQHTWD'
+description 'A simple plug and play resource for players to know your server keys.'
+
+client_scripts {
+    'client.lua',
+}
+
+print("^1fivem-rockstart-editor - By MiraxxR#0001 - https://github.com/MiraxxR1/fivem-rockstart-editor - https://discord.gg/wJmnKQHTWD")
